9fcc60e50e7a80d67053e1f2ee64647464408fc7
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-01-16 13:13:01 +0100

    Ensure cache keys are valid

--- src/PsrCacheFileFetcher.php
+++ src/PsrCacheFileFetcher.php
@@ -20,10 +20,28 @@ class PsrCacheFileFetcher implements FileFetcher {
 
 	private $fileFetcher;
 	private $cache;
+	private $keyBuilder;
 
-	public function __construct( FileFetcher $fileFetcher, CacheInterface $cache ) {
+	/**
+	 * @param FileFetcher $fileFetcher
+	 * @param CacheInterface $cache
+	 * @param callable|null $keyBuilderFunction Gets the fileUrl (string) and needs to return a valid cache key (string)
+	 */
+	public function __construct( FileFetcher $fileFetcher, CacheInterface $cache, callable $keyBuilderFunction = null ) {
 		$this->fileFetcher = $fileFetcher;
 		$this->cache = $cache;
+		$this->keyBuilder = $keyBuilderFunction ?? $this->getDefaultKeyBuilder();
+	}
+
+	private function getDefaultKeyBuilder(): callable {
+		return function( string $fileUrl ): string {
+			return preg_replace(
+					'/[^A-Za-z0-9\-]/',
+					'_',
+					$fileUrl
+				)
+				. '-' . substr( sha1( $fileUrl ), 0, 5 );
+		};
 	}
 
 	/**
@@ -42,18 +60,22 @@ class PsrCacheFileFetcher implements FileFetcher {
 
 	private function getFileContentsFromCache( string $fileUrl ): ?string {
 		try {
-			return $this->cache->get( $fileUrl );
+			return $this->cache->get( $this->createCacheKey( $fileUrl ) );
 		}
 		catch ( CacheException $ex ) {
 			return null;
 		}
 	}
 
+	private function createCacheKey( string $fileUrl ): string {
+		return ( $this->keyBuilder )( $fileUrl );
+	}
+
 	private function retrieveAndCacheFile( $fileUrl ): string {
 		$fileContents = $this->fileFetcher->fetchFile( $fileUrl );
 
 		try {
-			$this->cache->set( $fileUrl, $fileContents );
+			$this->cache->set( $this->createCacheKey( $fileUrl ), $fileContents );
 		}
 		catch ( CacheException $ex ) {
 		}
--- tests/Unit/PsrCacheFileFetcherTest.php
+++ tests/Unit/PsrCacheFileFetcherTest.php
@@ -6,6 +6,7 @@ namespace FileFetcher\Tests\Unit;
 
 use FileFetcher\FileFetchingException;
 use FileFetcher\InMemoryFileFetcher;
+use FileFetcher\NullFileFetcher;
 use FileFetcher\PsrCacheFileFetcher;
 use FileFetcher\ThrowingFileFetcher;
 use PHPUnit\Framework\TestCase;
@@ -28,7 +29,10 @@ class PsrCacheFileFetcherTest extends TestCase {
 			new InMemoryFileFetcher( [
 				self::FILE_URL => self::FILE_CONTENT
 			] ),
-			$this->newNullCache()
+			$this->newNullCache(),
+			function( string $string ): string {
+				return $string;
+			}
 		);
 
 		$this->assertSame(
@@ -51,7 +55,10 @@ class PsrCacheFileFetcherTest extends TestCase {
 	public function testWhenFileIsCached_cachedContentsGetsReturned() {
 		$cachingFetcher = new PsrCacheFileFetcher(
 			new ThrowingFileFetcher(),
-			$this->newCacheWithFile()
+			$this->newCacheWithFile(),
+			function( string $string ): string {
+				return $string;
+			}
 		);
 
 		$this->assertSame(
@@ -85,14 +92,23 @@ class PsrCacheFileFetcherTest extends TestCase {
 			new InMemoryFileFetcher( [
 				self::FILE_URL => self::FILE_CONTENT
 			] ),
-			$cache
+			$cache,
+			function( string $string ): string {
+				return $string;
+			}
 		);
 
 		$cachingFetcher->fetchFile( self::FILE_URL );
 	}
 
 	public function testWhenFetcherThrowsException_itIsNotCaught() {
-		$fetcher = new PsrCacheFileFetcher( new ThrowingFileFetcher(), $this->newNullCache() );
+		$fetcher = new PsrCacheFileFetcher(
+			new ThrowingFileFetcher(),
+			$this->newNullCache(),
+			function( string $string ): string {
+				return $string;
+			}
+		);
 
 		$this->expectException( FileFetchingException::class );
 		$fetcher->fetchFile( self::FILE_URL );
@@ -149,6 +165,49 @@ class PsrCacheFileFetcherTest extends TestCase {
 			->willThrowException( $this->newCacheException() );
 
 		return $cache;
+	}
+
+	/**
+	 * @dataProvider cacheKeyProvider
+	 */
+	public function testCacheKeyBuilding( string $fileUrl, string $expectedCacheKey ) {
+		$cache = $this->createMock( CacheInterface::class );
+
+		$cache->expects( $this->once() )
+			->method( 'get' )
+			->with( $this->equalTo( $expectedCacheKey ) );
+
+		$cache->expects( $this->once() )
+			->method( 'set' )
+			->with( $this->equalTo( $expectedCacheKey ) );
+
+		$cachingFetcher = new PsrCacheFileFetcher(
+			new InMemoryFileFetcher( [
+				$fileUrl => self::FILE_CONTENT
+			] ),
+			$cache
+		);
+
+		$cachingFetcher->fetchFile( $fileUrl );
+	}
+
+	public function cacheKeyProvider() {
+		yield [
+			'https://www.entropywins.wtf/blog/wp-json/wp/v2/posts?per_page=10',
+			'https___www_entropywins_wtf_blog_wp-json_wp_v2_posts_per_page_10-adbba'
+		];
+		yield [
+			'/tmp',
+			'_tmp-8c393'
+		];
+		yield [
+			'http://localhost:8042/kittens.jpg',
+			'http___localhost_8042_kittens_jpg-2f14e'
+		];
+		yield [
+			'ÆntrøpyWins',
+			'__ntr__pyWins-6e250'
+		];
 	}
 
 }
